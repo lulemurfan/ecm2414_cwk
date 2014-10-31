9baa9f77c2fe22b01813ac4ddda2b8a07f3d58a2
cf345 <cf345@exeter.ac.uk>
2014-10-31 11:06:29 +0000

    finito

--- prog1/__SHELL0.java
+++
@@ -1,8 +0,0 @@
-
-public class __SHELL0 extends bluej.runtime.Shell {
-public static void run() throws Throwable {
-
-java.lang.String[] __bluej_param0 = {"peb1.txt","peb1.txt","peb1.txt","7"} ;
-PebbleGame.main(__bluej_param0);
-
-}}
